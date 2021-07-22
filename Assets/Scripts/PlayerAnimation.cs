ab15888c6e8c112280f5ab274953ff75608dd5eb
Edwardbovich <karkadayn@gmail.com>
2021-07-22 18:48:14 +0300

    2
    This reverts commit fd42aab60eeea81dc965601e3855250979318e8d.

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    public Animator anim;
+   
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        
+    }
+    public void Jumpup()
+    {
+        anim.SetTrigger("Jumpup");
+        
+    }
+    public void JumpDown()
+    {
+        anim.SetTrigger("Jump");
+    }
+
+    public void Stop()
+    {
+        anim.SetBool("Idle", true);
+        
+    }
+    public void Stap()
+    {
+        anim.SetBool("Idle", true);
+    }
+}
---
+++ Assets/Scripts/water.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class water : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "cube")
+        {
+
+
+
+
+            other.gameObject.transform.parent.gameObject.GetComponent<PlayerControll>().pos.transform.position += new Vector3(0, 0.6f, 0);
+            other.gameObject.transform.parent.gameObject.GetComponent<PlayerControll>().RemoveCube();
+            other.gameObject.transform.parent = null;
+            Destroy(other.gameObject, 6);
+
+        }
+
+    }
+}
